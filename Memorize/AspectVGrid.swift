156327edb8d2597545ed262bdddeb0a5eb29bfd3
xmcnulty <6352496+xmcnulty@users.noreply.github.com>
2025-04-16 09:53:40 -0700

    Implemented custom AspectVGrid that sizes cards to fit on-screen in a V-Stack

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Xavier McNulty on 4/16/25.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
